775b565961685b85d086a951c6fb5b86de3a8f71
nmata010 <nicholas.matamoros@gmail.com>
2026-02-22 22:52:18 -0500

    workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/facts-dot-tax/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 8080,
