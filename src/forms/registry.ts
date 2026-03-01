b8637ba11492e29fa17285f58fc5a29fc9e5d21c
nmata010 <nicholas.matamoros@gmail.com>
2026-02-28 21:28:07 -0500

    schedule a & 1040 updates

--- src/forms/registry.ts
+++ src/forms/registry.ts
@@ -2,6 +2,7 @@ import type { ComponentType } from "react";
 import { Form1040 } from "./form-1040";
 import { FormTbd } from "./form-tbd";
 import { Schedule1 } from "./schedule-1";
+import { ScheduleA } from "./schedule-a";
 
 export interface FormEntry {
   id: string;
@@ -13,5 +14,6 @@ export interface FormEntry {
 export const FORMS: FormEntry[] = [
   { id: "form1040", label: "Form 1040", component: Form1040, xmlFile: "1040.xml" },
   { id: "schedule1", label: "Schedule 1", component: Schedule1, xmlFile: "schedule1.xml" },
+  { id: "scheduleA", label: "Schedule A", component: ScheduleA, xmlFile: "scheduleA.xml" },
   { id: "formtbd", label: "Form TBD", component: FormTbd, xmlFile: "formtbd.xml" },
 ];
